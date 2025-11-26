a34cff9995ef8bb8123768826193dfc78b23633f
Hamza Ali <hamza.ali.art3@gmail.com>
2025-11-26 15:44:09 +0500

    learning.

--- src/app/products/product-components/product-table/product-table.ts
+++ src/app/products/product-components/product-table/product-table.ts
@@ -10,14 +10,18 @@ import { productInterface } from '@app/products/interface/product-interface';
 import { ProductsHttpService } from '@app/products/product-services/products-http-service/products-http.service';
 import { DataServiceService } from '@app/user/user-services/user-profile-data-service/data-service.service';
 import { ProductAdd } from '../product-add/product-add';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-product-table',
-  imports: [MatTableModule, MatPaginatorModule, DatePipe, MatIconModule, MatButtonModule],
+  imports: [MatTableModule, MatPaginatorModule, DatePipe, MatIconModule, MatButtonModule, MatInputModule, MatFormFieldModule],
   templateUrl: './product-table.html',
   styleUrl: './product-table.scss',
 })
 export class ProductTable {
+
+  error:boolean = true;
 
   displayedColumns: string[] = ['id', 'name', 'price', 'category', 'description', 'actions'];
 
@@ -37,6 +41,10 @@ export class ProductTable {
   
     ngAfterViewInit() {
       this.dataSource.paginator = this.paginator;
+    }
+
+    onClick(){
+      this.error = !this.error;
     }
   
     getData() {
